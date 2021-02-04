118c3fe945560eb3190b8fb57134a1a617d1412e
张驰 <chi.zhang@weimob.com>
2021-02-04 10:45:28 +0800

    remove useless package

--- src/main/java/com/maxbill/core/desktop/Desktop.java
+++ src/main/java/com/maxbill/core/desktop/Desktop.java
@@ -17,7 +17,6 @@ import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
